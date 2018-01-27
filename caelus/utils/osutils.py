89ca016d9a7bf05625cc6f86ce0699957487dc40
Shreyas Ananthan <shreyas@umd.edu>
2018-01-27 09:02:08 -0500

    Add clean_directory utility.
    Recursively removes files and directories from a given directory unless
    it matches one of the shell wildcard patterns provided in
    preserve_patterns.

--- caelus/utils/osutils.py
+++ caelus/utils/osutils.py
@@ -6,7 +6,12 @@ Miscellaneous OS utilities
 """
 
 import os
+import fnmatch
+import shutil
+import logging
 from contextlib import contextmanager
+
+_lgr = logging.getLogger(__name__)
 
 def ostype():
     """String indicating the operating system type
@@ -68,6 +73,37 @@ def set_work_dir(dname, create=False):
     orig_dir = os.getcwd()
     try:
         os.chdir(abs_dir)
+        _lgr.debug("Setting work directory: %s", abs_dir)
         yield abs_dir
     finally:
         os.chdir(orig_dir)
+        _lgr.debug("Setting work directory: %s", abs_dir)
+
+def clean_directory(dirname,
+                    preserve_patterns=None):
+    """Utility function to remove files and directories from a given directory.
+
+    User can specify a list of filename patterns to preserve with the
+    ``preserve_patterns`` argument. These patterns can contain shell wildcards
+    to glob multiple files.
+
+    Args:
+        dirname (path): Absolute path to the directory whose entries are purged.
+        preserve_patterns (list): A list of shell wildcard patterns
+    """
+    _lgr.debug("Removing files in directory: %s", dirname)
+    ppatterns = preserve_patterns or []
+    with set_work_dir(dirname) as wdir:
+        for fpath in os.listdir(wdir):
+            is_preserve = False
+            for pp in ppatterns:
+                if fnmatch.fnmatch(fpath, pp):
+                    is_preserve = True
+                    break
+            if is_preserve:
+                continue
+
+            if os.path.isdir(fpath):
+                shutil.rmtree(fpath)
+            elif os.path.isfile(fpath) or os.path.islink(fpath):
+                os.remove(fpath)
