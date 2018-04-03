a4bbcb2d533755361d2c81466ccbc4d22f534cf8
Shreyas Ananthan <shreyas@umd.edu>
2018-04-02 21:06:00 -0600

    Add unit tests for OS utilities

--- tests/utils/test_osutils.py
+++ tests/utils/test_osutils.py
@@ -10,3 +10,15 @@ def test_ensure_directory(tmpdir):
     cname = str(casedir)
     newdir = osutils.ensure_directory(pth.join(cname, "test_dir"))
     assert pth.exists(newdir)
+
+def test_backup_file(tmpdir):
+    src = tmpdir.join("source.txt")
+    src.write("dummy")
+    dest_test = osutils.backup_file(str(src))
+    tstamp = osutils.timestamp("%Y%m%d-%H%M%S-%Z")
+    dest_expect = tmpdir.join("source_" + tstamp + ".txt")
+    assert(dest_test == dest_expect)
+
+def test_misc_osutils():
+    osutils.username()
+    osutils.user_home_dir()
