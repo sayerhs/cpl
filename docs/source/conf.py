d2a5161141c1ac43064adb355466965be322f6d7
Shreyas Ananthan <shreyas@umd.edu>
2018-04-13 14:33:45 -0600

    Fix read the docs settings for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,8 @@ import os
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+on_rtd = os.environ.get("READTHEDOCS") == 'True'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -119,8 +121,11 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx13'
-html_theme_path = ['_themes']
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx13'
+    html_theme_path = ['_themes']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
