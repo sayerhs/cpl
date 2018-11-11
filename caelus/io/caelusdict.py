cbaf1c4e3d946e227a01e490bb73976600930ae9
Shreyas Ananthan <shreyas@umd.edu>
2018-11-11 13:17:13 -0700

    Formatted output for CaelusDict

--- caelus/io/caelusdict.py
+++ caelus/io/caelusdict.py
@@ -5,7 +5,15 @@ Caelus/OpenFOAM Dictionary Implementation
 -----------------------------------------
 """
 
+import six
 from ..utils import struct
+from .printer import DictPrinter
 
 class CaelusDict(struct.Struct):
     """Caelus Input File Dictionary"""
+
+    def __str__(self):
+        strbuf = six.StringIO()
+        pprint = DictPrinter(strbuf)
+        pprint(self)
+        return strbuf.getvalue()
