1fdde8ef3b3a2dbc267ef4c4332d8058866bdc0f
Shreyas Ananthan <shreyas@umd.edu>
2019-11-10 10:48:40 -0700

    Temporary add script path to allow local imports

--- caelus/utils/pyutils.py
+++ caelus/utils/pyutils.py
@@ -5,6 +5,7 @@ Python script import utilities
 ------------------------------
 """
 
+import sys
 import os
 import importlib
 
@@ -17,8 +18,12 @@ def import_script(fname):
     if not os.path.exists(fpath):
         raise FileNotFoundError("Cannot find file: %s"%fname)
 
-    (_, modname, _) = osutils.split_path(fpath)
-    mspec = importlib.util.spec_from_file_location(modname, fpath)
-    module = importlib.util.module_from_spec(mspec)
-    mspec.loader.exec_module(module)
-    return module
+    (moddir, modname, _) = osutils.split_path(fpath)
+    try:
+        sys.path.append(moddir)
+        mspec = importlib.util.spec_from_file_location(modname, fpath)
+        module = importlib.util.module_from_spec(mspec)
+        mspec.loader.exec_module(module)
+        return module
+    finally:
+        sys.path.pop()
